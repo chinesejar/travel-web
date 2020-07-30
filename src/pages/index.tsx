7190381c5921fe0f8c36a515de065618f5bf5ce5
chinesejar <chinesejar@gmail.com>
2020-07-30 20:23:04 +0800

    update UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,17 +23,17 @@ export default () => {
         </Col>
       </Row>
       <Row>
-        <Col span={8} className={styles.box}>
+        <Col md={8} sm={24} className={styles.box}>
           <Typography.Title level={4}>GIS 攻略</Typography.Title>
           <Typography.Text>以 GIS 为基础，显示攻略</Typography.Text>
           <Trip1Svg height={300} />
         </Col>
-        <Col span={8} className={styles.box}>
+        <Col md={8} sm={24} className={styles.box}>
           <Typography.Title level={4}>智能推荐</Typography.Title>
           <Typography.Text>智能推荐攻略中的景点、住宿、游玩</Typography.Text>
           <Trip2Svg height={300} />
         </Col>
-        <Col span={8} className={styles.box}>
+        <Col md={8} sm={24} className={styles.box}>
           <Typography.Title level={4}>深度学习</Typography.Title>
           <Typography.Text>对各个攻略进行深度学习，提高智能化</Typography.Text>
           <Trip3Svg height={300} />
